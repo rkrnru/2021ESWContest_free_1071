53c2541eba5a83d4f02fc112256ef56f3fb23ef1
rkrnru <57119957+rkrnru@users.noreply.github.com>
2021-10-28 15:16:30 +0900

    Update measureHumidity.py

--- WaterControl/measureHumidity.py
+++ WaterControl/measureHumidity.py
@@ -13,20 +13,34 @@ class MeasureHumidity():
         self.HUM_MAX = 1023
 
         #Initial setting of motor drive
+        #기본 핀 설정이랑 다를 때, 경고 보내지 말라고 설정
         GPIO.setwarnings(False)
+        #BCM 모드로 핀 번호 다루기로 설정한다. BCM은 Broadcom 사의 SoC(System on Chip)에서 쓰는
+        #넘버링 체계이다.
         GPIO.setmode(GPIO.BCM)
-
+        #GPIO 핀 설정. 첫 parameter는 설정할 핀 번호, 두번째 것은 핀 모드. 23번 핀을 입력용으로 설정
         GPIO.setup(self.DIGIT,GPIO.IN)
 
+        #spi 디바이스 기능을 쓰기 위해서 객체 생성.
         self.spi = spidev.SpiDev()
+        #bus 열기. 첫 parameter는 버스 번호, 두번째 것은 device 번호
         self.spi.open(0,0)
+        #spi 통신을 위한 최대 클럭 설정.
         self.spi.max_speed_hz=50000
 
     #Function to get ADC value
     def read_spi_adc(self, adcChannel):
         adcValue = 0
         buff = self.spi.xfer2( [1, (8+adcChannel)<<4, 0] )
+        #spi transaction을 위한 설정. parameter는 list로써, MISO 핀으로 보낼 데이터.
+        #transaction은 말 그대로 교환이기 때문에, 리스트에 담긴 무의미한(최소한 여기선) 데이터가 수분 센서로 보내지고,
+        #그 대신 수분 센서 데이터가 반환된다.
+        #리스트 외에 기타 데이터를 추가해서 transaction 설정을 바꿀 수 있지만, 입력 안했음. default 설정 사용.
+        
         adcValue = ( (buff[1] & 3 ) << 8 ) + buff[2]
+        #marshalling. 2byte의 데이터를 정렬해서 가져오기. 라즈베리파이에서는 word 당 비트 수가 8로 설정된다.
+        #그래서, 8비트씩 쪼개서 가져온 걸 marshalling 한다.
+        
         adcValue = 1023-adcValue # without this, when wet data's 0, dry data's 1023
         adcValue = adcValue*1.55  # without this max data is around 820.
         adcValue = int(adcValue)
